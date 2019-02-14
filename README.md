2760e28abe83d1f87cd1b618aff9492ff14d1056
Leandro T <trinidad.leandro@protonmail.com>
2019-02-13 20:45:50 -0700

    finished the script

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# countDown
-# countDown
+# countDown anagram Solver
+This script uses the oxford dictionary to solve the anagram part
+of the countdown game show.
+
+## Updates
+
+### Febuary 13, 2019 - The Finish
+- Finished the script
+
+## Add more?
+- [ ] argument for how many words to be outputed
+- [ ] shows de0finition
+
--- countDown.py
+++ countDown.py
@@ -5,38 +5,62 @@ import os
 import urllib.request
 
 def help():
+# Prints out the help page #
     print("Help: crackerDown -a [text]")
-def wordInDic(word, anagram): #TODO: How do i do this !!!!!
-    letters = [x for x in anagram]
 
-    for let in letters:
+
+def wordInDic(word, anagram): #TODO: How do i do this !!!!!
+# Checks if the word can be created using the anagram #
+    word = word.lower()
+    anagram = anagram.lower()
+    for letter in word:
+        if(letter not in anagram):
+            return False
+        else:
+            anagram = anagram.replace(letter,"",1)
+    return True
+            
 
 def getAnagram():
+# gets the users arguments #
     argv = sys.argv
     return sys.argv[2]
 
+
+def sortArray(arry): 
+# Sorts the array #
+    arry.sort(key = len, reverse = True)
+
+
 def getDictionary(letter):
+# gets the dictionary words from my gihub page #
     url = 'https://raw.githubusercontent.com/Leandro-Trinidad-QECS/Oxford-Dictionary/master/'
     url3 = '.txt'
     data = urllib.request.urlopen(url + letter + url3)
 
     return [line.decode("utf-8").strip("\n") for line in data]
 
+
 def main():
+# main program #
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
-    #if((len(sys.argv) < 3) or sys.argv[1] != "-a"):
-     #   help()
-      #  exit(0)
+    if((len(sys.argv) < 3) or sys.argv[1] != "-a"):
+        help()
+        exit(0)
 
-
-    anagram = 'leandro'
+    anagram = getAnagram()
+    print("-"*5 + " countDown " + "-"*5)
+    print("Input: %s" %(anagram))
     solved = []
     for letter in alphabet:
         for word in getDictionary(letter):
             if(wordInDic(word,anagram)):
                 solved.append(word)
-   
-    print(solved)
+    sortArray(solved)
+
+    for i in range(5):
+        print(str("%s) %s %s" % (str(i+1), solved[i], len(solved[i]))))
+    print("-"*21)
 
 if(__name__=="__main__"):
     main()
